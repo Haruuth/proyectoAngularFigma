44e301c2bb22fc0bac96605a25c5439c696ecf99
Haruuth <haruuth@gmail.com>
2023-04-03 20:04:38 +0200

    3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SharedComponent } from './shared/shared.component';
 import { ModelsComponent } from './components/models/models.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { FormsModule } from '@angular/forms';
     SharedComponent,
     ModelsComponent,
     ProductosComponent,
+    FooterComponent,
 
   ],
   imports: [
